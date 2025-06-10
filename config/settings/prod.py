c2467e327c6a469fab5692b767eecb47c49f3740
roberto mazo <mazziitoozz@gmail.com>
2025-06-10 18:36:39 +0200

    modified prod.py

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,11 +1,10 @@
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 from .base import *
 
 DEBUG = False
-
 SECRET_KEY = os.environ["SECRET_KEY"]
-
 MONGO_CONNECTION_URI = os.environ["MONGO_CONNECTION_URI"]
-
 MONGO_DB_NAME = os.getenv("MONGO_DB_NAME", "autoppia")
