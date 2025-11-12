418f09aadb5ceaeac8c82f02677388c5312c849f
dr riiveer <59394472+Riiveer@users.noreply.github.com>
2025-11-12 18:58:16 +0100

    update

--- app/config.py
+++ app/config.py
@@ -101,7 +101,7 @@ class Settings(BaseSettings):
     ROUND_SIZE_EPOCHS: float = float(_env_var("ROUND_SIZE_EPOCHS", "2.0"))
     BLOCKS_PER_EPOCH: int = int(_env_var("BLOCKS_PER_EPOCH", "360"))
     DZ_STARTING_BLOCK: int = int(
-        _env_var("DZ_STARTING_BLOCK", "6832060")
+        _env_var("DZ_STARTING_BLOCK", "6873314")
     )  # Synced with validator config
 
     # Chain state
