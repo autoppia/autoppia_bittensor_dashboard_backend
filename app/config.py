fb20c15bc66bfe8b2b84b911aceb051d94e8cf85
dr riiveer <59394472+Riiveer@users.noreply.github.com>
2025-11-24 01:14:03 +0100

    update

--- app/config.py
+++ app/config.py
@@ -100,7 +100,7 @@ class Settings(BaseSettings):
     # ROUND_SIZE_EPOCHS_LOCAL, ROUND_SIZE_EPOCHS_DEVELOPMENT, etc.
     ROUND_SIZE_EPOCHS: float = float(_env_var("ROUND_SIZE_EPOCHS", "3.0"))
     BLOCKS_PER_EPOCH: int = int(_env_var("BLOCKS_PER_EPOCH", "360"))
-    DZ_STARTING_BLOCK: int = int(_env_var("DZ_STARTING_BLOCK", "6948689"))
+    DZ_STARTING_BLOCK: int = int(_env_var("DZ_STARTING_BLOCK", "6948988"))
 
     # Chain state
     CHAIN_BLOCK_CACHE_TTL_SECONDS: int = 15 * 60
